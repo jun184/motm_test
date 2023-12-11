ec0ab9d7ef094f9dfe3c3be59a310dc71145939e
SJ <0616po@gmail.com>
2023-12-11 16:11:36 +0900

    Update sw.js

--- script/sw.js
+++ script/sw.js
@@ -1,19 +1,1 @@
 self.addEventListener("fetch", e => {});
-
-let installPrompt = null;
-const installButton = document.querySelector("#install");
-
-window.addEventListener("beforeinstallprompt", (event) => {
-event.preventDefault();
-installPrompt = event;
-installButton.style.visibility = 'visible';
-});
-installButton.addEventListener("click", async () => {
-    if (!installPrompt) {
-        return;
-    }
-    const result = await installPrompt.prompt();
-    console.log(`Install prompt was: ${result.outcome}`);
-    installPrompt = null;
-    installButton.setAttribute("hidden", "");
-    });
