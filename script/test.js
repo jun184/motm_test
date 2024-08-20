8ad929d35b5c0c676cd9dc3cc6556e2fc7c68ab8
SJ <0616po@gmail.com>
2024-08-20 18:27:58 +0900

    Update test.js

--- script/test.js
+++ script/test.js
@@ -1,40 +1,40 @@
-var btnLocation = ['LT', 'LB', 'RT', 'RB']
-var btnTop
-var btnLeft
+var btn위치 = ['LT', 'LB', 'RT', 'RB']
+바 btn상단
+바 btn좌측
 if(btnRandom == true){
-    var selected = btnLocation[Math.floor(Math.random() * btnLocation.length)] // 랜덤하게 버튼 위치 설정
-}else if(!btnposition){
-    selected = 'RB'
-}else{
-    selected = btnposition
+ var 선택 = btnLocation [Math.floor (Math.random() * btnLocation.length)] // 랜덤하게 버튼 위치 설정
+ var 선택 = btnLocation [Math.floor (Math.random() * btnLocation.length)] // 랜덤하게 버튼 위치 설정
+} if(!btn position){ else
+ 선택한 = 'RB'
+ 선택한 = btn position
+ 선택한 = btn position
+
+if (선택된 == 'LT') { //왼쪽 위
+if (선택된 == 'LT') { //왼쪽 위
+ btnTop = AdHeight - (AdHeight - btnMargin)
+ btn왼쪽 = AdWidth - (AdWidth - btnMargin)
+}그렇지 않은 경우(선택된 == 'LB') { //왼쪽 아래
+ btnTop = 광고 높이 - (btnSize)높이 + btn마진)
+ btn왼쪽 = AdWidth - (AdWidth - btnMargin)
+}그렇지 않은 경우(선택된 == 'RT') { //오른쪽 상단)
+ btnTop = AdHeight - (AdHeight - btnMargin)
+ btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+ btnTop = 광고 높이 - (btnSize)높이 + btn마진)
+ btnTop = 광고 높이 - (btnSize)높이 + btn마진)
+ btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+
+문서.('<divid="all_wrapper1" style="position:absolute; left:0 px; top:0 px; width:0 px; high:0 px; text-align:center; margin:0 auto; z-index:98;")\n');
+문서.('<divid="second_wrapper1" style="위치:relative;좌:0 px;상단:0 px;문자-align:좌;폭:0 px;높이:0 px;마진:0 auto;")\n');
+문서.('<div=")라고 적습니다.FloatLayer1" STYLE="position:absolute; width:'+AdWidth+'px; 높이:'+AdHeight+'px; z-index:300; left:'+AdMarginLeft+'px; top:'+AdMargin상단+'px; 디스플레이:block;">\n');
+
+문서.('<IFRAME FRAME BORDER=0 MARGINTH=0 MARGINTH=0 SCROLLING=0 NO WIDTH='+AdWIDTH+'HIGH='+AdHeight+'SRC='+AdUrl+' allow Transparency="true"><<IFRAME>);
+문서.('<divid="close" style="position:absolute; top:'+btnTop+'px; left:'+btnLeft+'px; display:block;")\n');
+문서.쓰기('<a style="display:block;" href="jav 스크립트:flot_close ();"><img src="/advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg " width="'+btnSizeWidth+'" high="'+btnSize높이+'" 테두리 ="0" 스타일 ="디스플레이: 블록;"></a></div>\n');
+
+문서.('</div>\n)라고 적습니다.');
+문서.('</div>\n)라고 적습니다.');
+문서.('</div>\n)라고 적습니다.');
+
+기능. 플롯_클로즈() {
+ document.getElementById('all_wrapper1').style.display = 'none'; 
 }
-
-if (selected == 'LT') { //Left top
-    btnTop = AdHeight - (AdHeight - btnMargin)
-    btnLeft = AdWidth - (AdWidth - btnMargin)
-} else if (selected == 'LB') { //Left bottom
-    btnTop = AdHeight - (btnSizeHeight + btnMargin)
-    btnLeft = AdWidth - (AdWidth - btnMargin)
-} else if (selected == 'RT') { //Right top
-    btnTop = AdHeight - (AdHeight - btnMargin)
-    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
-} else { //Right bottom
-    btnTop = AdHeight - (btnSizeHeight + btnMargin)
-    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
-}
-
-document.write ('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:0px; height:0px; text-align:center; margin:0 auto; z-index:98;">\n');
-document.write ('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; text-align:left; width:0px; height:0px; margin:0 auto;">\n');
-document.write ('<div id="FloatLayer1" STYLE="position:absolute; width:'+AdWidth+'px; height:'+AdHeight+'px; z-index:300; left:'+AdMarginLeft+'px; top:'+AdMarginTop+'px; display:block;">\n');
-
-document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+AdWidth+'" HEIGHT="'+AdHeight+'" SRC="'+AdUrl+'" allowTransparency="true"></IFRAME>');
-document.write ('<div id="close" style="position:absolute; top:"'+btnTop+'"px; left:"'+btnLeft+'"px; display:block;">\n');
-document.write ('<a style="display: block;" href="javascript:flot_close();"><img src="//advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="'+btnSizeWidth+'" height="'+btnSizeHeight+'" border="0" style="display: block;"></a></div>\n');
-
-document.write ('</div>\n');
-document.write ('</div>\n');
-document.write ('</div>\n');
-
-function flot_close() {
-    	document.getElementById('all_wrapper1').style.display = 'none'; 
-    }
