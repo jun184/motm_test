fdd8d8d3632ef85abb1dcb2f41d82486b82e5ca5
SJ <0616po@gmail.com>
2024-08-20 19:24:45 +0900

    Update test.js

--- script/test.js
+++ script/test.js
@@ -11,24 +11,24 @@ if(btnRandom == true){
 }
 
 if (selected == 'LT') { //Left top
-    btnTop = AdHeight - (AdHeight - btnMargin)
-    btnLeft = AdWidth - (AdWidth - btnMargin)
+    btnTop = adHeight - (adHeight - btnMargin)
+    btnLeft = adWidth - (adWidth - btnMargin)
 } else if (selected == 'LB') { //Left bottom
-    btnTop = AdHeight - (btnSizeHeight + btnMargin)
-    btnLeft = AdWidth - (AdWidth - btnMargin)
+    btnTop = adHeight - (btnSizeHeight + btnMargin)
+    btnLeft = adWidth - (adWidth - btnMargin)
 } else if (selected == 'RT') { //Right top
-    btnTop = AdHeight - (AdHeight - btnMargin)
-    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+    btnTop = adHeight - (adHeight - btnMargin)
+    btnLeft = adWidth - (btnSizeWidth + btnMargin)
 } else { //Right bottom
-    btnTop = AdHeight - (btnSizeHeight + btnMargin)
-    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+    btnTop = adHeight - (btnSizeHeight + btnMargin)
+    btnLeft = adWidth - (btnSizeWidth + btnMargin)
 }
 
 document.write ('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:100%; height:0px; text-align:center; margin:0 auto; z-index:'+zIndex+';">\n');
 document.write ('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; width:0px; height:0px; text-align:left; margin:0 auto;">\n');
-document.write ('<div id="FloatLayer1" style="position:absolute; left:'+AdMarginLeft+'px; top:'+AdMarginTop+'px; width:'+AdWidth+'px; height:'+AdHeight+'px; display:block; z-index:300;">\n');
+document.write ('<div id="FloatLayer1" style="position:absolute; left:'+adMarginLeft+'px; top:'+adMarginTop+'px; width:'+adWidth+'px; height:'+adHeight+'px; display:block; z-index:300;">\n');
 
-document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+AdWidth+'" HEIGHT="'+AdHeight+'" SRC="'+AdUrl+'" allowTransparency="true"></IFRAME>');
+document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+adWidth+'" HEIGHT="'+adHeight+'" SRC="'+adUrl+'" allowTransparency="true"></IFRAME>');
 document.write ('<div id="close" style="position:absolute; top:'+btnTop+'px; left:'+btnLeft+'px; display:block;">\n');
 document.write ('<a style="display: block;" href="javascript:flot_close();"><img src="//advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="'+btnSizeWidth+'" height="'+btnSizeHeight+'" border="0" style="display: block;"></a></div>\n');
 
