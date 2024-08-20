2ce8c9e4bf3f101b0028e9f89a24048f7d2c3714
SJ <0616po@gmail.com>
2024-08-20 18:54:36 +0900

    Update test.js

--- script/test.js
+++ script/test.js
@@ -25,8 +25,8 @@ if (selected == 'LT') { //Left top
 }
 
 document.write ('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:100%; height:0px; text-align:center; margin:0 auto; z-index:98;">\n');
-document.write ('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; text-align:left; width:0px; height:0px; margin:0 auto;">\n');
-document.write ('<div id="FloatLayer1" STYLE="position:absolute; width:'+AdWidth+'px; height:'+AdHeight+'px; z-index:300; left:'+AdMarginLeft+'px; top:'+AdMarginTop+'px; display:block;">\n');
+document.write ('<div id="second_wrapper1" style="position:relative; left:'+AdMarginLeft+'px; top:0px; text-align:left; margin:0 auto;">\n');
+document.write ('<div id="FloatLayer1" STYLE="position:absolute; width:'+AdWidth+'px; height:'+AdHeight+'px; z-index:300; top:'+AdMarginTop+'px; display:block;">\n');
 
 document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+AdWidth+'" HEIGHT="'+AdHeight+'" SRC="'+AdUrl+'" allowTransparency="true"></IFRAME>');
 document.write ('<div id="close" style="position:absolute; top:'+btnTop+'px; left:'+btnLeft+'px; display:block;">\n');
