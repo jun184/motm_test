80e118d8843d9fff19a01292a07d62cb8b1f401c
SJ <0616po@gmail.com>
2024-08-20 18:22:50 +0900

    Update test.js
    버튼위치 추가

--- script/test.js
+++ script/test.js
@@ -1,10 +1,35 @@
+var btnLocation = ['LT', 'LB', 'RT', 'RB']
+var btnTop
+var btnLeft
+if(btnRandom == true){
+    var selected = btnLocation[Math.floor(Math.random() * btnLocation.length)] // 랜덤하게 버튼 위치 설정
+}else if(!btnposition){
+    selected = 'RB'
+}else{
+    selected = btnposition
+}
+
+if (selected == 'LT') { //Left top
+    btnTop = AdHeight - (AdHeight - btnMargin)
+    btnLeft = AdWidth - (AdWidth - btnMargin)
+} else if (selected == 'LB') { //Left bottom
+    btnTop = AdHeight - (btnSizeHeight + btnMargin)
+    btnLeft = AdWidth - (AdWidth - btnMargin)
+} else if (selected == 'RT') { //Right top
+    btnTop = AdHeight - (AdHeight - btnMargin)
+    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+} else { //Right bottom
+    btnTop = AdHeight - (btnSizeHeight + btnMargin)
+    btnLeft = AdWidth - (btnSizeWidth + btnMargin)
+}
+
 document.write ('<div id="all_wrapper1" style="position:absolute; left:0px; top:0px; width:0px; height:0px; text-align:center; margin:0 auto; z-index:98;">\n');
 document.write ('<div id="second_wrapper1" style="position:relative; left:0px; top:0px; text-align:left; width:0px; height:0px; margin:0 auto;">\n');
 document.write ('<div id="FloatLayer1" STYLE="position:absolute; width:'+AdWidth+'px; height:'+AdHeight+'px; z-index:300; left:'+AdMarginLeft+'px; top:'+AdMarginTop+'px; display:block;">\n');
 
 document.write ('<IFRAME FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH="'+AdWidth+'" HEIGHT="'+AdHeight+'" SRC="'+AdUrl+'" allowTransparency="true"></IFRAME>');
-document.write ('<div id="close" style="position:absolute; top:280px; left:280px; display:block;">\n');
-document.write ('<a style="display: block;" href="javascript:flot_close();"><img src="//advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="12" height="12" border="0" style="display: block;"></a></div>\n');
+document.write ('<div id="close" style="position:absolute; top:"'+btnTop+'"px; left:"'+btnLeft+'"px; display:block;">\n');
+document.write ('<a style="display: block;" href="javascript:flot_close();"><img src="//advimg.khan.co.kr/RealMedia/ads/Creatives/khan/js_upload_NEW/close_1212_black_article.jpg" width="'+btnSizeWidth+'" height="'+btnSizeHeight+'" border="0" style="display: block;"></a></div>\n');
 
 document.write ('</div>\n');
 document.write ('</div>\n');
